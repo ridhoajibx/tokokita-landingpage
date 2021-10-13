c8ea1cf6c91888b4d4fd2af23cedad88074ea4ac
ridhoajibx <ridhoajibx@gmail.com>
2021-10-13 12:25:59 +0700

    [ridho] - setup router

--- src/App.js
+++ src/App.js
@@ -3,15 +3,38 @@ import "styles/globalStyles.css";
 import React from "react";
 import { css } from "styled-components/macro"; //eslint-disable-line
 
-// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+// Pages
 import Home from "pages/Home";
+import AboutUs from "pages/AboutUs";
+import ContactUs from "pages/ContactUs";
+import Blog from "pages/BlogIndex";
+
 
 export default function App() {
-  // If you want to disable the animation just use the disabled `prop` like below on your page's component
-  // return <AnimationRevealPage disabled>xxxxxxxxxx</AnimationRevealPage>;
+    // If you want to disable the animation just use the disabled `prop` like below on your page's component
+    // return <AnimationRevealPage disabled>xxxxxxxxxx</AnimationRevealPage>;
 
 
-  return (
-      <Home />
-  );
+    return (
+        <Router>
+            <div>
+                <Switch>
+                    <Route path="/news">
+                        <Blog />
+                    </Route>
+                    <Route path="/about-us">
+                        <AboutUs />
+                    </Route>
+                    <Route path="/contact-us">
+                        <ContactUs />
+                    </Route>
+                    <Route path="/">
+                        <Home />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
